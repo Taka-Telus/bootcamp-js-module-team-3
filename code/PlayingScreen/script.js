bd37c8427a513faec4dd95fe52de9f0808a36bf1
Giovanni La Volpe <giovanni.lavolpe@gm2dev.com>
2026-07-13 10:51:13 -0300

    feat(script.js) add obtenerGame and mostrarPregunta

--- code/PlayingScreen/script.js
+++ code/PlayingScreen/script.js
@@ -1,61 +1,97 @@
-const game1 = {
-  "id": "g1",
-  "title": "Mundial 2026",
-  "author": "Cátedra",
-  "image": "https://digitalhub.fifa.com/transform/598a1d22-62b6-486b-849c-e8bf55894179/FIFA_FWC26_Tournament-Thumbnail-4-3",
-  "questions": [
-    {
-      "id": "q1",
-      "text": "¿Quién ganó el Mundial 2022?",
-      "options": [
-        "Argentina",
-        "Francia",
-        "Brasil",
-        "Croacia"
-      ]
-    },
-    {
-      "id": "q2",
-      "text": "¿En qué países se juega el Mundial 2026?",
-      "options": [
-        "Estados Unidos, Canadá y México",
-        "Qatar",
-        "Rusia",
-        "Brasil"
-      ]
+
+
+
+const baseUrl = "https://quiz-api.cesar-kastli.workers.dev";
+let preguntaActual = 0; // variable para asignar la posición de cada pregunta.
+
+const section = document.getElementById("section");
+
+let indice = 2
+
+async function obtenerGame() {
+    const response = await fetch(`${baseUrl}/games`);      // Obtiene la lista de juegos
+    const games = await response.json();                   // Convierte la respuesta a JSON, sin esto no se puede acceder a los datos de la respuesta.
+    const gameId = games[indice]?.id;                           // crea gameId con el id del objeto indicado
+
+    if (!gameId) {                                         // si no hay id, significa que no hay juegos.
+        section.innerHTML = "<h2>No se encontró ningún juego.</h2>";
+        return;
     }
-  ],
-  "difficulty": "Fácil"
+
+    const gameResponse = await fetch(`${baseUrl}/games/${gameId}`); // Obtiene el juego con el id especificado
+    const game = await gameResponse.json();                         // Convierte la respuesta a JSON, y nombra 'game' al objeto.
+
+    mostrarPregunta(game); // Esta funcion muestra cada pregunta con sus respuestas.
 }
 
-// pregunta 1
-const question = document.createElement("p");
-question.textContent = game1.questions[0].text;
-const section = document.getElementById("section");
-section.appendChild(question);
+obtenerGame();  // Llama a la funcion para obtener el juego y mostrar la primera pregunta.
 
-// opciones 1
-const opciones = document.createElement("ul");
-section.appendChild(opciones);
+function mostrarPregunta(game) {
 
-game1.questions[0].options.forEach((textoOpcion) => {
-    const li = document.createElement("li");
-    li.textContent = textoOpcion;
-    opciones.appendChild(li);
+    section.innerHTML = ""; // limpia el html de ''section'' por lo tanto, borra todo lo anterior.
+
+    // Pregunta
+    const question = document.createElement("p"); // crea la pregunta como un <p>
+    question.textContent = game.questions[preguntaActual].text;
+    section.appendChild(question);
+
+    const respuesta = document.createElement("p");
+    respuesta.style.display = "none";
+    question.after(respuesta);
+    respuesta.classList.add("mensaje-respuesta")
+
+    // Opciones
+    const opciones = document.createElement("ul"); // crea la <ul>
+    section.appendChild(opciones);
 
 
-    li.addEventListener("click", () => {
+    let respondida = false; // evita que se pueda responder mas de una vez.
 
-    let respuestaCorrecta;
+    game.questions[preguntaActual].options.forEach((textoOpcion) => {
 
-    if (textoOpcion === game1.questions[0].options[0]) {
-        respuestaCorrecta = true;
-        console.log("Correcto");
-        li.classList.add('correcto')
-    } else {
-        respuestaCorrecta = false;
-        console.log("correcto");
-        li.classList.add('incorrecto')
-    }
+        const li = document.createElement("li");    // crea los <li>
+        li.textContent = textoOpcion;
+        opciones.appendChild(li);
+
+        li.addEventListener("click", () => {
+
+            if (respondida) return;
+
+            respondida = true;
+            
+            if (textoOpcion === game.questions[preguntaActual].options[0]) {
+                console.log("Correcto");
+                li.classList.add("correcto");
+                respuesta.textContent = "Respuesta correcta!";
+                respuesta.style.display = "block";
+                respuesta.classList.add("mensaje-respuesta-correcta")
+            } else {
+                console.log("Incorrecto");
+                li.classList.add("incorrecto");
+                opciones.children[0].classList.add("correcto");
+                respuesta.textContent = "Respuesta incorrecta!";
+                respuesta.style.display = "block";
+                respuesta.classList.add("mensaje-respuesta-incorrecta")
+            }
+
+            const button = document.createElement("button");
+            button.textContent = "Siguiente pregunta";
+            section.appendChild(button);
+
+            button.addEventListener("click", () => {
+
+                preguntaActual++; // pasa a la sig pregunta.
+
+                if (preguntaActual < game.questions.length) {
+                    mostrarPregunta(game); // Empieza denuevo la funcion para mostrar la sig pregunta.
+                } else {
+                    section.innerHTML = "<h2>¡Quiz finalizado!</h2>";
+                }
+
+            });
+
+        });
+
     });
-});
+
+}
